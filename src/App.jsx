b988ededa5098d14559c47d9ba4687f43cf13a43
JpSpectre85 <140466447+JpSpectre85@users.noreply.github.com>
2024-06-24 16:08:06 -0500

    uso de prettier en formato

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,51 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import useFetch from './hooks/useFetch'
-import InfoLocation from './components/InfoLocation'
-import CardResident from './components/CardResident'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import InfoLocation from "./components/InfoLocation";
+import CardResident from "./components/CardResident";
 
 function App() {
+  const [locationId, setLocationId] = useState(
+    Math.floor(Math.random() * 126) + 1
+  );
 
-  const [locationId, setLocationId] = useState(Math.floor(Math.random()*126) + 1)
+  const url = `https://rickandmortyapi.com/api/location/${locationId}`;
+  const [location, getLocation, isLoading, hasError] = useFetch(url);
 
-  const url = `https://rickandmortyapi.com/api/location/${locationId}`
-  const [location, getLocation, isLoading, hasError ]= useFetch(url)
+  useEffect(() => {
+    getLocation();
+  }, [locationId]);
 
-  useEffect(() =>{
-    getLocation()
-  }, [locationId])
+  const inputLocation = useRef();
 
-
-
-  const inputLocation = useRef()
-
-  const handleLocation = e  =>{
-    e.preventDefault()
-    setLocationId(inputLocation.current.value.trim())
-  }
+  const handleLocation = (e) => {
+    e.preventDefault();
+    setLocationId(inputLocation.current.value.trim());
+  };
 
   return (
-
-    <div className='app'>
-      <img src="/banner.png" alt="" className='app_banner'/>
-      <form className='app_form' onSubmit={handleLocation}>
-        <input className='app_input' ref={inputLocation} type="text" />
-        <button className='app_btn'>Search</button>
+    <div className="app">
+      <img src="/banner.png" alt="" className="app_banner" />
+      <form className="app_form" onSubmit={handleLocation}>
+        <input className="app_input" ref={inputLocation} type="text" />
+        <button className="app_btn">Search</button>
       </form>
-      {
-        isLoading
-        ? <h2>Loading...</h2>
-        :(
-        hasError || locationId === "0"
-          ? <h2>Error, You must provide an id form 1 to 126</h2>
-          : (
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : hasError || locationId === "0" ? (
+        <h2>Error, You must provide an id form 1 to 126</h2>
+      ) : (
         <>
-          <InfoLocation  location={location}/>
-            <div className='app_card-container'> 
-              {
-                location?.residents.map(url =>(
-                  <CardResident 
-                  key={url}
-                  url={url}
-                  />
-                ))
-              }
-            </div>
+          <InfoLocation location={location} />
+          <div className="app_card-container">
+            {location?.residents.map((url) => (
+              <CardResident key={url} url={url} />
+            ))}
+          </div>
         </>
-          )
-          )
-      }
+      )}
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,27 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
 const useFetch = (url) => {
+  const [infoApi, setInfoApi] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-    const [infoApi, setInfoApi ] = useState ()
-    const [isLoading, setIsLoading] = useState(true)
-    const [hasError, setHasError] = useState(false)
+  const getApi = () => {
+    setIsLoading(true);
+    axios
+      .get(url)
+      .then((res) => {
+        setInfoApi(res.data);
+        setHasError(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setHasError(true);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-    const getApi = () => {
-        setIsLoading(true)
-        axios.get(url)
-            .then( res => {
-                setInfoApi(res.data)
-                setHasError(false)
-            })
-            .catch(err => {
-                console.log(err) 
-                setHasError(true)
-            })
-            .finally(() =>setIsLoading(false))
-    }
+  return [infoApi, getApi, isLoading, hasError];
+};
 
-    return [ infoApi, getApi, isLoading, hasError]
-}
-
-export default useFetch
+export default useFetch;
